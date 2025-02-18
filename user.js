f9e5f1772d321de686081e809ebb1cb8480062e9
aura <100294874+auratmos@users.noreply.github.com>
2025-02-19 01:44:14 +1100

    Create 'user.js'

---
+++ user.js
@@ -0,0 +1,17 @@
+// user.js
+// https://github.com/auratmos/user.js
+
+user_pref("privacy.resistFingerprinting", true) // Learn more: https://support.mozilla.org/en-US/kb/resist-fingerprinting
+user_pref("privacy.firstparty.isolate", true)
+
+// Privacy and security
+user_pref("webgl.disabled", true)
+
+// Sidebar
+user_pref("sidebar.revamp", true)
+user_pref("sidebar.verticalTabs", true)
+user_pref("sidebar.main.tools", "")
+
+// Windows Mica style
+user_pref("browser.tabs.allow_transparent_browser", true)
+user_pref("widget.windows.mica", true)
